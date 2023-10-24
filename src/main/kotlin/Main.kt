6b2d24bc7ac1c7ac4a1605f19d9fed037b12f43f
Llarence <1010kai.t1010@gmail.com>
2023-10-23 22:02:21 -0700

    add header to calendar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,10 +3,20 @@ import com.calendarfx.model.CalendarSource
 import com.calendarfx.model.Entry
 import com.calendarfx.model.Interval
 import com.calendarfx.view.CalendarView
+import com.sun.javafx.util.Utils.clamp
 import javafx.application.Application
 import javafx.application.Application.launch
 import javafx.application.Platform
+import javafx.event.Event
+import javafx.event.EventHandler
+import javafx.event.EventType
 import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.control.TextField
+import javafx.scene.layout.HBox
+import javafx.scene.layout.Region
+import javafx.scene.layout.VBox
+import javafx.scene.text.Text
 import javafx.stage.Stage
 import kotlinx.coroutines.*
 import kotlinx.coroutines.javafx.JavaFx
@@ -22,19 +32,64 @@ fun LocalDate.withScheduleWeekday(day: Int): LocalDate {
     return this.plusDays((day + 1).mod(7).toLong() - this.dayOfWeek.value)
 }
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class App : Application() {
-    private val calendar: Calendar<Nothing>
+    private lateinit var stage: Stage
+
+    private val loadScene: Scene
+
+    private val loadText = Text()
+
     private val calendarScene: Scene
 
-    init {
-        val calendarView = CalendarView()
-        val source = CalendarSource("Schedule")
-        calendar = Calendar<Nothing>("")
+    private val calendar: Calendar<Nothing>
+    val nameField = TextField()
 
+    var calendarNameFormat = ""
+
+    lateinit var deferredSchedules: Deferred<List<Schedule>>
+    var scheduleIndex = 0
+
+    init {
+        val loadVBox = VBox()
+
+        loadVBox.children.add(loadText)
+
+        loadScene = Scene(loadVBox)
+    }
+
+    init {
+        val calendarVBox = VBox()
+
+        val headerHBox = HBox()
+
+        val prevButton = Button("Previous")
+        prevButton.onAction = EventHandler { shiftSchedule(-1) }
+
+        val nextButton = Button("Next")
+        nextButton.onAction = EventHandler { shiftSchedule(1) }
+
+        nameField.textProperty().addListener { _, _, new ->
+            setCalendarName(new)
+        }
+
+        headerHBox.children.addAll(prevButton, nextButton, nameField)
+
+        val calendarView = CalendarView()
+        calendarVBox.heightProperty().addListener { _, _, new ->
+            calendarView.prefHeight = new.toDouble()
+        }
+
+        val source = CalendarSource("Schedule")
+
+        calendar = Calendar<Nothing>("")
         source.calendars.add(calendar)
+
         calendarView.calendarSources.add(source)
 
-        calendarScene = Scene(calendarView)
+        calendarVBox.children.addAll(headerHBox, calendarView)
+
+        calendarScene = Scene(calendarVBox)
 
         fxScope.launch {
             while (true) {
@@ -45,8 +100,74 @@ class App : Application() {
         }
     }
 
+    private fun setSceneCalendar() {
+        fxScope.launch {
+            deferredSchedules = coroutineScope.async {
+                genSchedule(listOf(
+                    Pair(listOf("MTH 311", "MTH 343", "MTH 351"), 0.1),
+                    Pair(listOf("MTH 311", "MTH 343", "MTH 351"), 0.1),
+                    Pair(listOf("COMM 114"), 0.1),
+                    Pair(listOf("ENGR 103"), 0.1),
+                    Pair(listOf("PH 211"), 0.1),
+                    Pair(listOf("ANTH 284", "BI 102", "BI 103", "BI 204"), 0.1),
+                    Pair(listOf("ED 216", "ED 219", "ENG 220", "ENG 260"), 0.1),
+                    Pair(listOf("ATS 342", "GEO 308", "GEOG 300", "H 312"), 0.1),
+                    Pair(listOf("ANTH 330", "ART 367", "ES 319", "FES 485"), 0.1)
+                ), Term.WINTER, 4000, genScheduleGrader(listOf(), 0.0, 1.0))
+            }
+
+            loadText.text = "Loading"
+            val showLoad = launch {
+                while (true) {
+                    loadText.text = "Loading"
+                    delay(300)
+
+                    loadText.text = "Loading."
+                    delay(300)
+
+                    loadText.text = "Loading.."
+                    delay(300)
+
+                    loadText.text = "Loading..."
+                    delay(300)
+                }
+            }
+
+            stage.scene = loadScene
+
+            deferredSchedules.await()
+
+            showLoad.cancelAndJoin()
+
+            scheduleIndex = 0
+            val first = deferredSchedules.getCompleted().firstOrNull()
+            if (first != null) {
+                setCalendar(scheduleIndex.toString(), first)
+            }
+
+            stage.scene = calendarScene
+        }
+    }
+
+    private fun setCalendarName(name: String) {
+        calendar.name = calendarNameFormat.format(name)
+        nameField.text = name
+    }
+
+    private fun shiftSchedule(change: Int) {
+        val schedules = deferredSchedules.getCompleted()
+
+        if (schedules.isEmpty()) {
+            return
+        }
+
+        scheduleIndex = (scheduleIndex + change).mod(schedules.size)
+        setCalendar(scheduleIndex.toString(), schedules[scheduleIndex])
+    }
+
     private fun setCalendar(name: String, schedule: Schedule) {
-        calendar.name = "Name: $name\nCredits: ${schedule.credits}\nGrade: ${schedule.grade}"
+        calendarNameFormat = "Name: %s\nCredits: ${schedule.credits}\nGrade: ${schedule.grade}"
+        setCalendarName(name)
 
         calendar.clear()
         val now = LocalDate.now()
@@ -68,33 +189,18 @@ class App : Application() {
     }
 
     override fun start(stage: Stage) {
+        this.stage = stage
+
         stage.title = "Calendar"
-        stage.setScene(calendarScene)
         stage.isMaximized = true
         stage.centerOnScreen()
-
-        val deferredSchedules = coroutineScope.async {
-            genSchedule(listOf(
-                Pair(listOf("MTH 311", "MTH 343", "MTH 351"), 0.1),
-                Pair(listOf("MTH 311", "MTH 343", "MTH 351"), 0.1),
-                Pair(listOf("COMM 114"), 0.1),
-                Pair(listOf("ENGR 103"), 0.1),
-                Pair(listOf("PH 211"), 0.1),
-                Pair(listOf("ANTH 284", "BI 102", "BI 103", "BI 204"), 0.1),
-                Pair(listOf("ED 216", "ED 219", "ENG 220", "ENG 260"), 0.1),
-                Pair(listOf("ATS 342", "GEO 308", "GEOG 300", "H 312"), 0.1),
-                Pair(listOf("ANTH 330", "ART 367", "ES 319", "FES 485"), 0.1)
-            ), Term.WINTER, 4000, genScheduleGrader(listOf(), 0.0, 1.0))
-        }
-
-        fxScope.launch {
-            setCalendar("1", deferredSchedules.await().first())
-        }
 
         stage.setOnCloseRequest {
             Platform.exit()
             exitProcess(0)
         }
+
+        setSceneCalendar()
 
         stage.show()
     }
