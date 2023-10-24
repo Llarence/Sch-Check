d7f506321e68eab21cfc4866742c022a4b11c589
Llarence <1010kai.t1010@gmail.com>
2023-10-23 21:01:38 -0700

    refactor main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,26 +6,73 @@ import com.calendarfx.view.CalendarView
 import javafx.application.Application
 import javafx.application.Application.launch
 import javafx.application.Platform
-import javafx.event.EventHandler
 import javafx.scene.Scene
 import javafx.stage.Stage
-import javafx.stage.WindowEvent
 import kotlinx.coroutines.*
 import kotlinx.coroutines.javafx.JavaFx
+import java.io.File
 import java.time.LocalDate
 import java.time.LocalTime
-import kotlin.math.min
 import kotlin.system.exitProcess
 
 val coroutineScope = CoroutineScope(Dispatchers.Default)
 val fxScope = CoroutineScope(Dispatchers.JavaFx)
 
-fun LocalDate.withWeekday(day: Int): LocalDate {
-    return this.plusDays(day.toLong() - this.dayOfWeek.value)
+fun LocalDate.withScheduleWeekday(day: Int): LocalDate {
+    return this.plusDays((day + 1).mod(7).toLong() - this.dayOfWeek.value)
 }
 
 class App : Application() {
+    private val calendar: Calendar<Nothing>
+    private val calendarScene: Scene
+
+    init {
+        val calendarView = CalendarView()
+        val source = CalendarSource("Schedule")
+        calendar = Calendar<Nothing>("")
+
+        source.calendars.add(calendar)
+        calendarView.calendarSources.add(source)
+
+        calendarScene = Scene(calendarView)
+
+        fxScope.launch {
+            while (true) {
+                calendarView.time = LocalTime.now()
+                calendarView.today = LocalDate.now()
+                delay(1000)
+            }
+        }
+    }
+
+    private fun setCalendar(name: String, schedule: Schedule) {
+        calendar.name = "Name: $name\nCredits: ${schedule.credits}\nGrade: ${schedule.grade}"
+
+        calendar.clear()
+        val now = LocalDate.now()
+        for (classDatum in schedule.classData) {
+            for (meetTime in classDatum.meetingTimes) {
+                val date = now.withScheduleWeekday(meetTime.meetDay)
+
+                val startHours = meetTime.startTime.inWholeHours.toInt()
+                val endHours = meetTime.endTime.inWholeHours.toInt()
+
+                val interval = Interval(
+                    date.atTime(startHours, meetTime.startTime.inWholeMinutes.toInt() - (startHours * 60)),
+                    date.atTime(endHours, meetTime.endTime.inWholeMinutes.toInt() - (endHours * 60))
+                )
+
+                calendar.addEntry(Entry<Nothing>(classDatum.title, interval))
+            }
+        }
+    }
+
     override fun start(stage: Stage) {
+        stage.title = "Calendar"
+        stage.setScene(calendarScene)
+        stage.isMaximized = true
+        stage.centerOnScreen()
+
         val deferredSchedules = coroutineScope.async {
             genSchedule(listOf(
                 Pair(listOf("MTH 311", "MTH 343", "MTH 351"), 0.1),
@@ -40,51 +87,9 @@ class App : Application() {
             ), Term.WINTER, 4000, genScheduleGrader(listOf(), 0.0, 1.0))
         }
 
-        val calendarView = CalendarView()
-        val source = CalendarSource("Schedule")
-
         fxScope.launch {
-            val schedules = deferredSchedules.await()
-            for ((schedule, credits, grade) in schedules) {
-                // It doesn't give more precise times than in minutes
-                val minutes = schedule.sumOf { meetings -> meetings.meetingTimes.sumOf { (it.endTime - it.startTime).inWholeMinutes } }
-                val hours = minutes / 60
-                val calendar = Calendar("Classes: ${schedule.size} Time: $hours:${minutes - (hours * 60)} Credits: $credits Grade: $grade", null)
-
-                for (classData in schedule) {
-                    for (time in classData.meetingTimes) {
-                        val now = LocalDate.now().withWeekday((time.meetDay + 1).mod(7))
-                        val startHours = time.startTime.inWholeHours.toInt()
-                        val endHours = time.endTime.inWholeHours.toInt()
-                        val interval = Interval(
-                            now.atTime(startHours, time.startTime.inWholeMinutes.toInt() - (startHours * 60)),
-                            now.atTime(endHours, time.endTime.inWholeMinutes.toInt() - (endHours * 60))
-                        )
-
-                        calendar.addEntry(Entry<Nothing?>(classData.title, interval))
-                    }
-                }
-
-                source.calendars.add(calendar)
-            }
+            setCalendar("1", deferredSchedules.await().first())
         }
-
-        fxScope.launch {
-            while (true) {
-                calendarView.time = LocalTime.now()
-                calendarView.today = LocalDate.now()
-                delay(1000)
-            }
-        }
-
-        calendarView.calendarSources.add(source)
-
-        val scene = Scene(calendarView)
-
-        stage.title = "Calendar"
-        stage.setScene(scene)
-        stage.isMaximized = true
-        stage.centerOnScreen()
 
         stage.setOnCloseRequest {
             Platform.exit()
