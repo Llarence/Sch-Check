af46c37dad9be84b958055460a73f4e130eb9ca2
Llarence <1010kai.t1010@gmail.com>
2024-01-22 20:31:54 -0800

    added argument saving and loading

--- src/main/kotlin/ArgumentPage.kt
+++ src/main/kotlin/ArgumentPage.kt
@@ -9,7 +9,7 @@ import java.io.File
 import java.time.DayOfWeek
 
 @Serializable
-data class ScheduleGenArguments(val classGroupSearches: List<List<Search>>, val tries: Int, val skipChance: Double)
+data class ScheduleGenArguments(val classGroupsSearches: List<List<Search>>, val tries: Int, val skipChance: Double)
 
 private val argumentSaver = Saver.create<ScheduleGenArguments>(File("saves/arguments/"))
 private val optionStringConverter = object : StringConverter<Option>() {
@@ -35,7 +35,7 @@ class DayTimePicker : VBox() {
     }
 }
 
-class SearchPane(val term: Option) : VBox() {
+class SearchPane(private val term: Option, default: Search = Search(term = term.code)) : VBox() {
     private val subject = ComboBox<Option>()
     private val courseNumber = Spinner<Int>(0, Int.MAX_VALUE, Int.MIN_VALUE)
     private val keyword = TextField()
@@ -69,26 +69,44 @@ class SearchPane(val term: Option) : VBox() {
     init {
         val searchOptions = searchOptions[term]!!
 
+        if (default.courseNumber != null) { courseNumber.valueFactory.value = default.courseNumber }
+        if (default.keyword != null) { keyword.text = default.keyword }
+        if (default.keywordAll != null) { keywordAll.text = default.keywordAll }
+        if (default.keywordAny != null) { keywordAny.text = default.keywordAny }
+        if (default.keywordExact != null) { keywordExact.text = default.keywordExact }
+        if (default.keywordWithout != null) { keywordWithout.text = default.keywordWithout }
+        if (default.durationValue != null) { durationValue.valueFactory.value = default.durationValue }
+        if (default.courseNumberRangeLow != null) { courseNumberRangeLow.valueFactory.value = default.courseNumberRangeLow }
+        if (default.courseNumberRangeHigh != null) { courseNumberRangeHigh.valueFactory.value = default.courseNumberRangeHigh }
+        monday.isSelected = DayOfWeek.MONDAY in default.days
+        tuesday.isSelected = DayOfWeek.TUESDAY in default.days
+        wednesday.isSelected = DayOfWeek.WEDNESDAY in default.days
+        thursday.isSelected = DayOfWeek.THURSDAY in default.days
+        friday.isSelected = DayOfWeek.FRIDAY in default.days
+        saturday.isSelected = DayOfWeek.SATURDAY in default.days
+        sunday.isSelected = DayOfWeek.SUNDAY in default.days
+        openOnly.isSelected = default.openOnly
+
         children.addAll(
-            addEnable(labelComboBox(subject, "Subject", searchOptions.subjects)),
-            addEnable(Label("Course Number", courseNumber)),
-            addEnable(Label("Keyword", keyword)),
-            addEnable(Label("Keyword All", keywordAll)),
-            addEnable(Label("Keyword Any", keywordAny)),
-            addEnable(Label("Keyword Exact", keywordExact)),
-            addEnable(Label("Keyword Without", keywordWithout)),
-            addEnable(labelComboBox(attribute, "Attribute", searchOptions.attributes)),
-            addEnable(labelComboBox(campus, "Campus", searchOptions.campuses)),
-            addEnable(labelComboBox(level, "Level", searchOptions.levels)),
-            addEnable(labelComboBox(building, "Building", searchOptions.buildings)),
-            addEnable(labelComboBox(college, "College", searchOptions.colleges)),
-            addEnable(labelComboBox(department, "Department", searchOptions.departments)),
-            addEnable(labelComboBox(scheduleType, "Schedule Type", searchOptions.scheduleTypes)),
-            addEnable(Label("Duration Value", durationValue)),
-            addEnable(labelComboBox(durationType, "Duration Type", searchOptions.durationTypes)),
-            addEnable(labelComboBox(partOfTerm, "Part Of Term", searchOptions.partsOfTerm)),
-            addEnable(Label("Course Number Low", courseNumberRangeLow)),
-            addEnable(Label("Course Number High", courseNumberRangeHigh)),
+            addEnable(labelComboBox(subject, "Subject", searchOptions.subjects, default.subject), default.subject != null),
+            addEnable(Label("Course Number", courseNumber), default.courseNumber != null),
+            addEnable(Label("Keyword", keyword), default.keyword != null),
+            addEnable(Label("Keyword All", keywordAll), default.keywordAll != null),
+            addEnable(Label("Keyword Any", keywordAny), default.keywordAny != null),
+            addEnable(Label("Keyword Exact", keywordExact), default.keywordExact != null),
+            addEnable(Label("Keyword Without", keywordWithout), default.keywordWithout != null),
+            addEnable(labelComboBox(attribute, "Attribute", searchOptions.attributes, default.attribute), default.attribute != null),
+            addEnable(labelComboBox(campus, "Campus", searchOptions.campuses, default.campus), default.campus != null),
+            addEnable(labelComboBox(level, "Level", searchOptions.levels, default.level), default.level != null),
+            addEnable(labelComboBox(building, "Building", searchOptions.buildings, default.building), default.building != null),
+            addEnable(labelComboBox(college, "College", searchOptions.colleges, default.college), default.college != null),
+            addEnable(labelComboBox(department, "Department", searchOptions.departments, default.department), default.department != null),
+            addEnable(labelComboBox(scheduleType, "Schedule Type", searchOptions.scheduleTypes, default.scheduleType), default.scheduleType != null),
+            addEnable(Label("Duration Value", durationValue), default.courseNumberRangeLow != null),
+            addEnable(labelComboBox(durationType, "Duration Type", searchOptions.durationTypes, default.durationType), default.durationType != null),
+            addEnable(labelComboBox(partOfTerm, "Part Of Term", searchOptions.partsOfTerm, default.partOfTerm), default.partOfTerm != null),
+            addEnable(Label("Course Number Low", courseNumberRangeLow), default.courseNumberRangeLow != null),
+            addEnable(Label("Course Number High", courseNumberRangeHigh), default.courseNumberRangeHigh != null),
             monday,
             tuesday,
             wednesday,
@@ -96,23 +114,34 @@ class SearchPane(val term: Option) : VBox() {
             friday,
             saturday,
             sunday,
-            addEnable(start),
-            addEnable(end),
+            addEnable(start, default.start != null),
+            addEnable(end, default.end != null),
             openOnly
         )
     }
 
-    private fun labelComboBox(comboBox: ComboBox<Option>, label: String, options: List<Option>): Label {
+    private fun labelComboBox(comboBox: ComboBox<Option>, label: String, options: List<Option>, default: String?): Label {
         comboBox.items.addAll(options)
-        comboBox.selectionModel.select(0)
+
+        if (default == null) {
+            comboBox.selectionModel.select(0)
+        } else {
+            comboBox.selectionModel.select(options.first { it.code == default })
+        }
+
         comboBox.converter = optionStringConverter
         return Label(label, comboBox)
     }
 
-    private fun addEnable(node: Node): HBox {
-        node.isDisable = true
-
+    private fun addEnable(node: Node, enabled: Boolean): HBox {
         val checkBox = CheckBox()
+        if (enabled) {
+            node.isDisable = false
+            checkBox.isSelected = true
+        } else {
+            node.isDisable = true
+        }
+
         checkBox.selectedProperty().addListener { _, _, value ->
             node.isDisable = !value
         }
@@ -131,28 +160,28 @@ class SearchPane(val term: Option) : VBox() {
         if (sunday.isSelected) { days.add(DayOfWeek.SUNDAY) }
 
         return Search(
-            subject = if (subject.isDisable) { null } else { subject.value.code },
-            courseNumber = if (courseNumber.isDisable) { null } else { courseNumber.value },
-            keyword = if (keyword.isDisable) { null } else { keyword.text },
-            keywordAll = if (keywordAll.isDisable) { null } else { keywordAll.text },
-            keywordAny = if (keywordAny.isDisable) { null } else { keywordAny.text },
-            keywordExact = if (keywordExact.isDisable) { null } else { keywordExact.text },
-            keywordWithout = if (keywordWithout.isDisable) { null } else { keywordWithout.text },
-            attribute = if (attribute.isDisable) { null } else { attribute.value.code },
-            campus = if (campus.isDisable) { null } else { campus.value.code },
-            level = if (level.isDisable) { null } else { level.value.code },
-            building = if (building.isDisable) { null } else { building.value.code },
-            college = if (college.isDisable) { null } else { college.value.code },
-            department = if (department.isDisable) { null } else { department.value.code },
-            scheduleType = if (scheduleType.isDisable) { null } else { scheduleType.value.code },
-            durationValue = if (durationValue.isDisable) { null } else { durationValue.value },
-            durationType = if (durationType.isDisable) { null } else { durationType.value.code },
-            partOfTerm = if (partOfTerm.isDisable) { null } else { partOfTerm.value.code },
-            courseNumberRangeLow = if (courseNumberRangeLow.isDisable) { null } else { courseNumberRangeLow.value },
-            courseNumberRangeHigh = if (courseNumberRangeHigh.isDisable) { null } else { courseNumberRangeHigh.value },
+            subject = if (subject.isDisabled) { null } else { subject.value.code },
+            courseNumber = if (courseNumber.isDisabled) { null } else { courseNumber.value },
+            keyword = if (keyword.isDisabled) { null } else { keyword.text },
+            keywordAll = if (keywordAll.isDisabled) { null } else { keywordAll.text },
+            keywordAny = if (keywordAny.isDisabled) { null } else { keywordAny.text },
+            keywordExact = if (keywordExact.isDisabled) { null } else { keywordExact.text },
+            keywordWithout = if (keywordWithout.isDisabled) { null } else { keywordWithout.text },
+            attribute = if (attribute.isDisabled) { null } else { attribute.value.code },
+            campus = if (campus.isDisabled) { null } else { campus.value.code },
+            level = if (level.isDisabled) { null } else { level.value.code },
+            building = if (building.isDisabled) { null } else { building.value.code },
+            college = if (college.isDisabled) { null } else { college.value.code },
+            department = if (department.isDisabled) { null } else { department.value.code },
+            scheduleType = if (scheduleType.isDisabled) { null } else { scheduleType.value.code },
+            durationValue = if (durationValue.isDisabled) { null } else { durationValue.value },
+            durationType = if (durationType.isDisabled) { null } else { durationType.value.code },
+            partOfTerm = if (partOfTerm.isDisabled) { null } else { partOfTerm.value.code },
+            courseNumberRangeLow = if (courseNumberRangeLow.isDisabled) { null } else { courseNumberRangeLow.value },
+            courseNumberRangeHigh = if (courseNumberRangeHigh.isDisabled) { null } else { courseNumberRangeHigh.value },
             days = days,
-            start = if (start.isDisable) { null } else { start.getValue() },
-            end = if (end.isDisable) { null } else { end.getValue() },
+            start = if (start.isDisabled) { null } else { start.getValue() },
+            end = if (end.isDisabled) { null } else { end.getValue() },
             openOnly = openOnly.isSelected,
             term = term.code
         )
@@ -174,23 +203,63 @@ fun addAddTab(tabPane: TabPane, tabGen: () -> Tab) {
 }
 
 class ArgumentPage(private val term: Option) : Page() {
-    init {
-        val tab = genClassGroupTab()
-        tab.isClosable = false
+    private val tabPane = TabPane()
 
-        val tabPane = TabPane()
+    init {
+        val saveLoadPanel =  SaveLoadPanel()
+        saveLoadPanel.onSave = { argumentSaver.save(it, getArgument()) }
+        saveLoadPanel.onLoad = {
+            val argument = argumentSaver.load(it)
+            if (argument != null) {
+                setArgument(argument)
+            }
+        }
+
+        val tab = genClassGroupTab()
+
         tabPane.tabs.add(tab)
         addAddTab(tabPane, ::genClassGroupTab)
 
-        mainVBox.children.addAll(tabPane)
+        mainVBox.children.addAll(saveLoadPanel, tabPane)
     }
 
-    private fun genClassGroupTab(): Tab {
-        val subTab = genSearchTab()
-        subTab.isClosable = false
+    // Hate all the casting
+    private fun getArgument(): ScheduleGenArguments {
+        val classGroupsSearches = mutableListOf<List<Search>>()
+        for (i in 0..<tabPane.tabs.size - 1) {
+            val classGroupSearches = mutableListOf<Search>()
 
+            val subTabPane = tabPane.tabs[i].content as TabPane
+            for (j in 0..<subTabPane.tabs.size - 1) {
+                classGroupSearches.add((subTabPane.tabs[j].content as SearchPane).getSearch())
+            }
+
+            classGroupsSearches.add(classGroupSearches)
+        }
+
+        return ScheduleGenArguments(classGroupsSearches, 1000, 0.2)
+    }
+
+    // Hate all the casting
+    private fun setArgument(argument: ScheduleGenArguments) {
+        val size = tabPane.tabs.size - 1
+
+        for (classGroupSearches in argument.classGroupsSearches.reversed()) {
+            tabPane.tabs.add(size, genClassGroupTab(classGroupSearches.reversed().map { genSearchTab(it) }))
+        }
+
+        // Stops the add tab from being selected and trigger a new tab being created
+        for (i in 0..<size) {
+            tabPane.tabs.remove(tabPane.tabs[0])
+        }
+    }
+
+    private fun genClassGroupTab(tabs: List<Tab>? = null): Tab {
         val subPane = TabPane()
-        subPane.tabs.add(subTab)
+        if (tabs != null) {
+            subPane.tabs.addAll(tabs)
+        }
+
         addAddTab(subPane, ::genSearchTab)
 
         val tab = Tab("Class Group")
@@ -199,9 +268,9 @@ class ArgumentPage(private val term: Option) : Page() {
         return tab
     }
 
-    private fun genSearchTab(): Tab {
+    private fun genSearchTab(search: Search = Search(term = term.code)): Tab {
         val tab = Tab("Search")
-        tab.content = SearchPane(term)
+        tab.content = SearchPane(term, search)
 
         return tab
     }
